5be57a27cfee51533fda55f6c1746dd81463d423
Sylvain Estevez <sylvain@luxe.com>
2017-11-16 17:45:16 -0800

    :rotating_light: More unit tests

---
+++ test/classes/access-control.test.ts
@@ -0,0 +1,24 @@
+import { Subject, AccessControl, MemoryStore, Access } from '../../';
+
+describe('AccessControl', () => {
+  class TestSubject extends Subject<{ id: number }> {
+    getPrincipal() { return this.get('id'); }
+  }
+
+  const store = new MemoryStore();
+  const accessControl = new AccessControl({ store });
+  const subject = new TestSubject({ id: 1 });
+
+  describe('#authorize()', () => {
+    it('should return an access', async () => {
+      const access = await accessControl.authorize(subject, 'foo', 'bar', {});
+      expect(access).to.be.instanceOf(Access);
+    });
+  });
+  describe('#can()', () => {
+    it('should return the access result', async () => {
+      const can = await accessControl.can(subject, 'foo', 'bar', {});
+      expect(can).to.be.equal(false);
+    });
+  });
+});
